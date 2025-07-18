5c91309d6ea76114ec8f03acc853bb89ed5df485
Pricecito <matias.gutierrez.1254@gmail.com>
2025-07-17 19:03:11 -0500

    Archivo Iniciales

---
+++ src/test/java/com/scraping/farmacos/FarmacosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scraping.farmacos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmacosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
