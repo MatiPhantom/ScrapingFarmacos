6915fb166ac2540cb63b74fb85fc6c5d3fcf9f0d
Pricecito <matias.gutierrez.1254@gmail.com>
2025-07-17 19:52:45 -0500

    Probando Conexion

--- src/main/java/com/scraping/farmacos/scraper/InkafarmaScraper.java
+++ src/main/java/com/scraping/farmacos/scraper/InkafarmaScraper.java
@@ -1,17 +1,34 @@
 package com.scraping.farmacos.scraper;
 
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import org.springframework.stereotype.Component;
 
 import com.scraping.farmacos.model.Producto;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Component
+@Slf4j
 public class InkafarmaScraper {
 
-    public Producto buscar(String nombre){
+    public void buscar(String nombreProducto){
         Producto producto = new Producto();
-        producto.setNombre(nombre);
+        producto.setNombre(nombreProducto);
 
-        
+        String url= "https://inkafarma.pe/buscador?keyword=" + URLEncoder.encode(nombreProducto, StandardCharsets.UTF_8);
+
+        try{
+            //Conectando a la URL 
+            Document doc = Jsoup.connect(url).get();
+            log.info("Conectado a la URL: " + url);
+        }catch(Exception e){
+            log.error("Conexion fallida a la URL: "+url, e);
+        }
+
     }
 
 }
