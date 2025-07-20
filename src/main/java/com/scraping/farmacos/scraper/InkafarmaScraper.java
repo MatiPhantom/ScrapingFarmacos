d4b1598d2e923498867ba02848dedf5c5267379c
Pricecito <matias.gutierrez.1254@gmail.com>
2025-07-19 22:28:50 -0500

    Extrayendo data JSON

--- src/main/java/com/scraping/farmacos/scraper/InkafarmaScraper.java
+++ src/main/java/com/scraping/farmacos/scraper/InkafarmaScraper.java
@@ -1,5 +1,6 @@
 package com.scraping.farmacos.scraper;
 
+import java.io.File;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
 import java.time.Duration;
@@ -19,6 +20,8 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.stereotype.Component;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.scraping.farmacos.model.Producto;
 
 import lombok.extern.slf4j.Slf4j;
@@ -27,74 +30,124 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class InkafarmaScraper {
 
+    private final ObjectMapper mapper= new ObjectMapper();
+    
+
     public void buscar(String nombreProducto) {
 
         // 1. Adicionando propiedad del driver de Chrome
-        System.setProperty("webdriver.chrome.driver", "C:\\drivers\\chromedriver.exe");
+        String driverPath = new File("src/main/resources/driver/chromedriver.exe").getAbsolutePath();
+        System.setProperty("webdriver.chrome.driver", driverPath);
 
         // 2. Usa Brave como navegador
         ChromeOptions options = new ChromeOptions();
-        options.setBinary("C:\\Program Files\\BraveSoftware\\Brave-Browser\\Application\\brave.exe"); // Ajusta si está
         options.addArguments("--headless=new");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
-        // en otro lugar
 
         // 3. Inicia el navegador controlado por Selenium
         WebDriver driver = new ChromeDriver(options);
 
         // 4. Abirendo conexión con la URL
-        driver.get("https://inkafarma.pe/buscador?keyword=paracetamol");
+        driver.get(
+                "https://inkafarma.pe/buscador?keyword=" + URLEncoder.encode(nombreProducto, StandardCharsets.UTF_8));
 
         // 5. Determinando tiempo de espera maximo para encontrar los elementos
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
 
-        try {
-            // 6. Configurando selector para encontrar los contenedores de productos
-            String selectorContenedor = "fp-filtered-product-list div.col-12[class*='col-sm-6']";
-            wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector(selectorContenedor)));
-            List<WebElement> contenedoresDeProductos = driver.findElements(By.cssSelector(selectorContenedor));
-            System.out
-                    .println("Se encontraron " + contenedoresDeProductos.size() + " contenedores de productos únicos.");
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.cssSelector("div.card.product")));
 
-            for (WebElement contenedor : contenedoresDeProductos) {
-                // Dentro de cada contenedor, busca la información del producto.
-                // No importa si encuentra la versión grande o pequeña, los datos son los
-                // mismos.
-                WebElement tarjetaProducto = contenedor.findElement(By.cssSelector("div.card.product"));
+        // Encuentra la primera tarjeta de producto que coincida
+        WebElement webProducto = driver.findElement(By.cssSelector("div.card.product"));
 
-                String nombre = tarjetaProducto.getAttribute("data-product-name");
-                String marca = tarjetaProducto.getAttribute("data-product-brand");
+        // Extrae los datos del producto
+        String dataProducto = webProducto.getAttribute("data-product");
+        try{
+            JsonNode jsonProducto = mapper.readTree(dataProducto);
+            Producto producto= new Producto();
+            producto.setNombre(jsonProducto.get("name").asText());
+            producto.setPrecio(jsonProducto.get("presentations").get(0).get("price").asText());
+            producto.setLaboratorio(jsonProducto.get("brand").asText());
+            producto.setCodigoDigemid(jsonProducto.get("sapCode").asText());
+            producto.setFuente("Inkafarma");
 
-                String precio = "";
-                String url = "";
+            
 
-                try {
-                    WebElement precioEl = tarjetaProducto.findElement(By.cssSelector("span.card-monedero span"));
-                    precio = precioEl.getText();
-                } catch (Exception e) {
-                    precio = "No disponible";
-                }
-
-                try {
-                    WebElement enlace = tarjetaProducto.findElement(By.cssSelector("a.link"));
-                    url = enlace.getAttribute("href");
-                } catch (Exception e) {
-                    url = "Sin URL";
-                }
-
-                System.out.println("Nombre: " + nombre);
-                System.out.println("Marca: " + marca);
-                System.out.println("Precio: " + precio);
-                System.out.println("URL: " + url);
-                System.out.println("---------");
-            }
-
-        } catch (Exception e) {
-            // 6. Cierra el navegador
+        }catch(Exception e){
+            log.error("Error al extraer datos del producto: ", e);
             driver.quit();
-            log.error("Error al buscar productos: ", e);
+            return;
         }
+        
+
+
+        /*
+         * String laboratorio = producto.getAttribute("data-product-brand");
+         * String urlProducto =
+         * producto.findElement(By.tagName("a")).getAttribute("href");
+         * 
+         * Producto info = new Producto();
+         * info.setNombre(nombre);
+         * info.setPrecio(precio);
+         * info.setLaboratorio(laboratorio);
+         * info.setFuente("Inkafarma"+ );
+         * log.info("Producto encontrado: " + info.toString() + " URL: " + urlProducto);
+         */
+
+        /*
+         * try {
+         * // 6. Configurando selector para encontrar los contenedores de productos
+         * String selectorContenedor =
+         * "fp-filtered-product-list div.col-12[class*='col-sm-6']";
+         * wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.cssSelector(
+         * selectorContenedor)));
+         * List<WebElement> contenedoresDeProductos =
+         * driver.findElements(By.cssSelector(selectorContenedor));
+         * System.out
+         * .println("Se encontraron " + contenedoresDeProductos.size() +
+         * " contenedores de productos únicos.");
+         * 
+         * for (WebElement contenedor : contenedoresDeProductos) {
+         * // Dentro de cada contenedor, busca la información del producto.
+         * // No importa si encuentra la versión grande o pequeña, los datos son los
+         * // mismos.
+         * WebElement tarjetaProducto =
+         * contenedor.findElement(By.cssSelector("div.card.product"));
+         * 
+         * String nombre = tarjetaProducto.getAttribute("data-product-name");
+         * String marca = tarjetaProducto.getAttribute("data-product-brand");
+         * 
+         * String precio = "";
+         * String url = "";
+         * 
+         * try {
+         * WebElement precioEl =
+         * tarjetaProducto.findElement(By.cssSelector("span.card-monedero span"));
+         * precio = precioEl.getText();
+         * } catch (Exception e) {
+         * precio = "No disponible";
+         * }
+         * 
+         * try {
+         * WebElement enlace = tarjetaProducto.findElement(By.cssSelector("a.link"));
+         * url = enlace.getAttribute("href");
+         * } catch (Exception e) {
+         * url = "Sin URL";
+         * }
+         * 
+         * System.out.println("Nombre: " + nombre);
+         * System.out.println("Marca: " + marca);
+         * System.out.println("Precio: " + precio);
+         * System.out.println("URL: " + url);
+         * System.out.println("---------");
+         * }
+         * 
+         * } catch (Exception e) {
+         * // 6. Cierra el navegador
+         * driver.quit();
+         * log.error("Error al buscar productos: ", e);
+         * }
+         */
 
     }
 
